7576aa6001800cdf605edfba0b4255148faed3d9
Erandi Madawalagama <89267499+erandime@users.noreply.github.com>
2023-09-23 18:00:05 +0800

    Update Project01.java

--- Project01.java
+++ Project01.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaPractice;
-
 import java.util.Scanner;
-
 /**
  *
  * @author erandi
  */
 public class Project01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);        
         System.out.println("Enter an Integer");
