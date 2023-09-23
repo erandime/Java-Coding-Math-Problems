b920cb18552a6d3744e7994cfa706384737512be
Erandi Madawalagama <89267499+erandime@users.noreply.github.com>
2023-09-23 17:59:07 +0800

    Add files via upload
    Ask the user to enter a set of real numbers and print the sum of those numbers on screen.

---
+++ Project02.java
@@ -0,0 +1,22 @@
+//package javaPractice;
+
+import java.util.Scanner;
+
+public class Project02 {
+
+    public static void main(String[] args) {
+        double product = 1.0;
+        double num;
+        Scanner myObj = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a Number OR enter 0 to end input: ");
+            num = myObj.nextDouble();
+            if(num == 0){break;}
+            product *= num;
+
+        }
+        System.out.println("Product of numbers: " + product);
+
+    }
+}
