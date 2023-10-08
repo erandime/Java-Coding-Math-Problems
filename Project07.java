647e9a835aa679f5492722ed325497c22468c53d
Erandi Madawalagama <89267499+erandime@users.noreply.github.com>
2023-10-09 01:59:39 +0800

    Update Project07.java

--- Project07.java
+++ Project07.java
@@ -1,24 +1,25 @@
-public class Project07 {
+import java.util.Scanner;
+
+public class Project04 {
 
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
-        int sum = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == j) {
-                    matrix[i][j] = i;
-                    sum = sum + i;
-                } else {
-                    matrix[i][j] = 0;
-                }
-            }
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter length of side 01: ");
+        double a = myObj.nextDouble();
+        System.out.println("Enter length of side 01: ");
+        double b = myObj.nextDouble();
+        System.out.println("Enter length of side 01: ");
+        double c = myObj.nextDouble();
+
+        if (a <= 0 && b <= 0 && c <= 0) {
+            System.out.println("Length cannot be zero.");
+        } else if (a + b <= c && b + c <= a && a + c <= b) {
+            System.out.println("Triangle doesn't exist");
+        } else {
+            double p = (a + b + c) / 2;
+            double area = Math.sqrt((p - a) * (p - b) * (p - c));
+            System.out.println("Perimeter of triangle is: " + p * 2);
+            System.out.println("Area of triangle is: " + Math.round(area * 100) / 100d);
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("Sum of diagonal elements is:" + sum);
     }
 }
