80afa229211dfa5fcb56c1c234757ddaf9604998
Erandi Madawalagama <89267499+erandime@users.noreply.github.com>
2023-10-18 01:25:02 +0800

    Update Project12.java

--- Project12.java
+++ Project12.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Project12 {
-
+    
     public static void main(String[] args) {
+        
         Scanner myScan = new Scanner(System.in);
         ArrayList<Integer> myList = new ArrayList<>();
 
-        while (true) {
-            System.out.println("Input integer (or any other character to stop input): ");
-            if (myScan.hasNextInt()) {
-                myList.add(myScan.nextInt());
-            } else {
-                break;
-            }
+        System.out.print("Input the number of integers/size of ArrayList ");
+        int num = myScan.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("Enter integer number " + (i + 1) + " : ");
+            myList.add(myScan.nextInt());
         }
-        
+
         System.out.println("Initial list is " + myList);
-        
+
         myList.replaceAll(e -> e * e);
         myList.removeIf(e -> e < 100);
         myList.replaceAll(e -> (int)Math.sqrt(e));
-        
+
         System.out.println("New list is " + myList);
     }
 }
